5b608a1e7d09cc8de074dd56d3725daf333eaaba
MaximBenkert <126075845+MaximBenkert@users.noreply.github.com>
2023-03-03 16:30:07 +0100

    modified passwordValidation

--- src/main/java/Passwordvalidation2.java
+++ src/main/java/Passwordvalidation2.java
@@ -1,35 +1,57 @@
 public class Passwordvalidation2 {
 
     public static void main(String[] args) {
-        System.out.println(passwordValidation("ölfklajdfl9akjsdf"));
+        System.out.println(passwordValidation("AADFDFSDSFASDF5"));
     }
-    public static String passwordValidation (String password) {
 
-        if (!passwordLength(password) && !containsDigit(password)){
+    public static String passwordValidation(String password) {
+
+        if (!passwordLength(password) && !containsDigit(password)) {
             return "your password must contain at least 8 characters and 1 digit";
         }
-        if (!containsDigit(password))
-        {
+        if (!containsDigit(password)) {
             return "your password must contain at least 1 digit";
         }
-        if (!passwordLength(password)){
+        if (!passwordLength(password)) {
             return "your password must contain at least 8 characters";
-        }else {
-            return "your password is valid";
         }
 
+            if (!isStringLowerCase(password)){
+                return "your password must contain at least 1 lower case character";
+        }
+            return "your password is valid";
+
+
     }
+
     public static boolean passwordLength(String password) {
         return password.length() >= 8;
     }
+
     public static boolean containsDigit(String password) {
 
-            for (int i = 0; i < password.length(); ++i) {
-                if (Character.isDigit(password.charAt(i))) {
-                    return true;
-                }
+        for (int i = 0; i < password.length(); ++i) {
+            if (Character.isDigit(password.charAt(i))) {
+                return true;
             }
-            return false;
+        }
+        return false;
+
+    }
+
+    private static boolean isStringLowerCase(String password) {
+
+        //convert String to char array
+        char[] charArray = password.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            //if any character is not in lower case, return false
+            if (!Character.isLowerCase(charArray[i]))
+                return false;
+        }
+
+        return true;
 
     }
 }
